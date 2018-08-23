e46cbbcbd85d18046c83c25b9f78dadf8dc73bea
Aaron Goldman <agoldman789@gmail.com>
2018-08-23 15:34:46 -0400

    Set up database creation script in schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -15,4 +15,66 @@ CREATE TABLE app_user (
   salt varchar(255) NOT NULL
 );
 
+CREATE TABLE place(
+google_place_id text,
+coffee_shop_name varchar(256) NOT NULL,
+address varchar(256) NOT NULL,
+photo_reference text NOT NULL,
+constraint pk_place primary key(google_place_id)
+);
+
+CREATE TABLE coffee(
+coffee_id serial,
+coffee_name varchar(256) NOT NULL,
+origin varchar(256) NOT NULL,
+roaster varchar(256) NOT NULL,
+detail text,
+constraint pk_coffee primary key(coffee_id)
+); 
+
+CREATE TABLE review(
+review_id serial,
+rating int NOT NULL,
+detail text NOT NULL,
+review_date Date NOT NULL DEFAULT CURRENT_DATE,
+constraint pk_review primary key(review_id)
+)
+
+CREATE TABLE place_coffee(
+google_place_id text,
+coffee_id int,
+  CONSTRAINT pk_place_coffee PRIMARY KEY (google_place_id, coffee_id),
+  CONSTRAINT fkey_google_place_id FOREIGN KEY (google_place_id)
+      REFERENCES place (google_place_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkey_coffee_id FOREIGN KEY (coffee_id)
+      REFERENCES coffee (coffee_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION 
+);
+
+
+CREATE TABLE coffee_review(
+coffee_id int,
+review_id int,
+ CONSTRAINT pk_coffee_review PRIMARY KEY (coffee_id, review_id),
+  CONSTRAINT fkey_coffee_id FOREIGN KEY (coffee_id)
+      REFERENCES coffee (coffee_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkey_review_id FOREIGN KEY (review_id)
+      REFERENCES review (review_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION 
+);
+
+CREATE TABLE user_review(
+user_id int,
+review_id int,
+ CONSTRAINT pk_user_review PRIMARY KEY (user_id, review_id),
+  CONSTRAINT fkey_user_id FOREIGN KEY (user_id)
+      REFERENCES app_user (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkey_review_id FOREIGN KEY (review_id)
+      REFERENCES review (review_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION 
+);
+
 COMMIT;
